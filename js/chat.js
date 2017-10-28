e5161f41a90ef638e6f67efc39cefbfaf4ee5ef0
sarthak268 <sarthakbhagat@Sarthaks-MacBook-Pro.local>
2017-10-28 09:17:27 +0530

    images

---
+++ js/chat.js
@@ -0,0 +1,9 @@
+$(function(){
+$("#addClass").click(function () {
+          $('#qnimate').addClass('popup-box-on');
+            });
+          
+            $("#removeClass").click(function () {
+          $('#qnimate').removeClass('popup-box-on');
+            });
+  })
